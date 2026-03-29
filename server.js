46e5944440a562e2360353b9bdd339a3a8030839
maxgogol228 <maxgogolonline@gmail.com>
2026-03-29 14:20:35 +0300

    Update server.js

--- server.js
+++ server.js
@@ -10,70 +10,144 @@ const io = socketIo(server, {
 
 app.use(express.static('public'));
 
-// Хранилище комнат и их участников
-const rooms = new Map(); // roomId -> { clients: Set(socketIds), offer: null, answer: null }
+// Store rooms and their participants
+const rooms = new Map();
 
 io.on('connection', (socket) => {
   console.log('Client connected:', socket.id);
 
-  // Создание новой комнаты
-  socket.on('create-room', (roomId, callback) => {
-    if (!rooms.has(roomId)) {
-      rooms.set(roomId, {
+  // Create or join room with custom key
+  socket.on('join-room', (roomKey, callback) => {
+    if (!rooms.has(roomKey)) {
+      // Create new room
+      rooms.set(roomKey, {
         clients: new Set([socket.id]),
-        initiator: socket.id
+        files: [] // Shared files queue
       });
-      socket.join(roomId);
-      socket.data.roomId = roomId;
-      callback({ success: true, isInitiator: true });
-      console.log(`Room ${roomId} created by ${socket.id}`);
+      socket.join(roomKey);
+      socket.data.roomKey = roomKey;
+      socket.data.isCreator = true;
+      callback({ success: true, isCreator: true, clientsCount: 1 });
+      console.log(`Room ${roomKey} created by ${socket.id}`);
     } else {
-      callback({ success: false, error: 'Room already exists' });
-    }
-  });
-
-  // Подключение к существующей комнате
-  socket.on('join-room', (roomId, callback) => {
-    const room = rooms.get(roomId);
-    if (room && room.clients.size === 1) {
+      // Join existing room
+      const room = rooms.get(roomKey);
       room.clients.add(socket.id);
-      socket.join(roomId);
-      socket.data.roomId = roomId;
+      socket.join(roomKey);
+      socket.data.roomKey = roomKey;
+      socket.data.isCreator = false;
       
-      // Уведомляем создателя комнаты, что кто-то подключился
-      io.to(room.initiator).emit('peer-joined', socket.id);
-      callback({ success: true, isInitiator: false });
-      console.log(`${socket.id} joined room ${roomId}`);
-    } else {
-      callback({ success: false, error: 'Room not available' });
+      // Notify all clients in room about new peer
+      io.to(roomKey).emit('peer-joined', {
+        peerId: socket.id,
+        clientsCount: room.clients.size
+      });
+      
+      callback({ success: true, isCreator: false, clientsCount: room.clients.size });
+      console.log(`${socket.id} joined room ${roomKey}`);
     }
   });
 
-  // Передача WebRTC signaling сообщений
-  socket.on('signal', (data) => {
-    const { to, signal } = data;
-    io.to(to).emit('signal', {
-      from: socket.id,
-      signal: signal
-    });
-  });
-
-  // Отключение клиента
-  socket.on('disconnect', () => {
-    const roomId = socket.data.roomId;
-    if (roomId && rooms.has(roomId)) {
-      const room = rooms.get(roomId);
+  // Leave room
+  socket.on('leave-room', () => {
+    const roomKey = socket.data.roomKey;
+    if (roomKey && rooms.has(roomKey)) {
+      const room = rooms.get(roomKey);
       room.clients.delete(socket.id);
       
-      // Уведомляем другого участника об отключении
-      const remainingClients = Array.from(room.clients);
-      remainingClients.forEach(clientId => {
-        io.to(clientId).emit('peer-disconnected');
+      // Notify others
+      socket.to(roomKey).emit('peer-left', {
+        peerId: socket.id,
+        clientsCount: room.clients.size
+      });
+      
+      socket.leave(roomKey);
+      
+      // Delete room if empty
+      if (room.clients.size === 0) {
+        rooms.delete(roomKey);
+        console.log(`Room ${roomKey} deleted`);
+      }
+    }
+  });
+
+  // Broadcast file metadata to all peers in room
+  socket.on('file-metadata', (data) => {
+    const roomKey = socket.data.roomKey;
+    if (roomKey) {
+      socket.to(roomKey).emit('file-metadata', {
+        fileId: data.fileId,
+        fileName: data.fileName,
+        fileSize: data.fileSize,
+        fileType: data.fileType,
+        senderId: socket.id
+      });
+    }
+  });
+
+  // Broadcast file chunk to specific peer or all peers
+  socket.on('file-chunk', (data) => {
+    const roomKey = socket.data.roomKey;
+    if (roomKey) {
+      if (data.targetId) {
+        // Send to specific peer
+        io.to(data.targetId).emit('file-chunk', {
+          fileId: data.fileId,
+          chunk: data.chunk,
+          offset: data.offset,
+          isLast: data.isLast
+        });
+      } else {
+        // Broadcast to all other peers
+        socket.to(roomKey).emit('file-chunk', {
+          fileId: data.fileId,
+          chunk: data.chunk,
+          offset: data.offset,
+          isLast: data.isLast,
+          senderId: socket.id
+        });
+      }
+    }
+  });
+
+  // Request file from peer
+  socket.on('request-file', (data) => {
+    const roomKey = socket.data.roomKey;
+    if (roomKey) {
+      io.to(data.peerId).emit('file-request', {
+        fileId: data.fileId,
+        requesterId: socket.id
+      });
+    }
+  });
+
+  // Get room info
+  socket.on('get-room-info', (roomKey, callback) => {
+    if (rooms.has(roomKey)) {
+      const room = rooms.get(roomKey);
+      callback({
+        exists: true,
+        clientsCount: room.clients.size,
+        clients: Array.from(room.clients)
+      });
+    } else {
+      callback({ exists: false });
+    }
+  });
+
+  socket.on('disconnect', () => {
+    const roomKey = socket.data.roomKey;
+    if (roomKey && rooms.has(roomKey)) {
+      const room = rooms.get(roomKey);
+      room.clients.delete(socket.id);
+      
+      socket.to(roomKey).emit('peer-left', {
+        peerId: socket.id,
+        clientsCount: room.clients.size
       });
       
       if (room.clients.size === 0) {
-        rooms.delete(roomId);
-        console.log(`Room ${roomId} deleted (empty)`);
+        rooms.delete(roomKey);
       }
     }
     console.log('Client disconnected:', socket.id);
